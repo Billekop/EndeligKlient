62fd8dba6b57b669595f34654297452c6d41b221
Alexander <alexanderskov@live.dk>
2016-11-24 14:56:25 +0100

    ny

--- src/models/user.java
+++ src/models/user.java
@@ -13,9 +13,81 @@ public class user {
     Boolean userType;
 
     public user(String firstName, String lastName, String userName, String email, String password, boolean userType){
-        
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public user(int userID, String firstName, String lastName, String userName, String email, String password, Boolean userType) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+    }
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    public void setUsername(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getUserType() {
+        return userType;
+    }
+
+    public void setUserType(Boolean admin) {
+        userType = admin;
     }
 
 
-
 }
+
+
+
