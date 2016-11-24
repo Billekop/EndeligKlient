b4ff1db3a1c61c5a3b0b0ad3774073f7e964224b
Alexander <alexanderskov@live.dk>
2016-11-24 16:00:36 +0100

    ny

--- src/sdk/HTTPrequest.java
+++ src/sdk/HTTPrequest.java
@@ -10,8 +10,7 @@ public class HTTPrequest {
     static Client client = Client.create();
 
 
-
-    public static ClientResponse get(String path){
+    public static ClientResponse get(String path) {
         ClientResponse clientResponse = null;
         try {
             WebResource webResource = client
@@ -19,18 +18,23 @@ public class HTTPrequest {
                     .path(path);
 
             clientResponse = webResource.accept("application/json").get(ClientResponse.class);
-        }
-        catch (UniformInterfaceException | ClientHandlerException e) {
+        } catch (UniformInterfaceException | ClientHandlerException e) {
             e.printStackTrace();
         }
         return clientResponse;
     }
 
-    public static ClientResponse post (String token, String path, String json){
+    public static ClientResponse post(String token, String path, String json) {
         ClientResponse clientResponse = null;
-        try{
+        try {
             WebResource webResource = client
                     .resource("http://localhost/server2_0_war_explored/")
+                    .path(path);
+            clientResponse = webResource.accept("application/json").get(ClientResponse.class);
+
+        } catch (UniformInterfaceException | ClientHandlerException e) {
+            e.printStackTrace();
         }
+        return clientResponse;
     }
 }
--- src/sdk/connection.java
+++ src/sdk/connection.java
@@ -13,12 +13,12 @@ import com.sun.jersey.api.client.ClientResponse;
 //denne klasse opretter forbindelse til serveren. Jeg har fået inspiration fra Jespers java-klient til at
     //lave denne klasse. Laver kald til serveren.
 public class connection {
-
+//laver et kald til serveren og tjekker i databasen under user login.
     public static String authorizeLogin(String username, String password) {
         userLogin userLogin = new userLogin(username, password);
-        ClientResponse clientResponse = HTTPrequests.post(null, "/user/login", new Gson().toJson(userLogin));
+        ClientResponse clientResponse = HTTPrequest.post(null, "/user/login", new Gson().toJson(userLogin));
         String token = null;
-
+// tjekker om der kommer respons fra serveren. else: ingen forbindelse
         if (clientResponse == null) {
             System.out.println("Der er ingen forbindelse til serveren");
         } else {
