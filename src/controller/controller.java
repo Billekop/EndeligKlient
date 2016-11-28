7fa2f1c0b7f2dad8b03d23f47c2ea00ce86577f8
Alexander <alexanderskov@live.dk>
2016-11-28 17:08:09 +0100

    ny

--- src/controller/controller.java
+++ src/controller/controller.java
@@ -38,16 +38,29 @@ Scanner input;
             do {
                 System.out.println("Login menu");
                 System.out.println("Tryk 1. for at printe en bog");
-                switch(input.nextInt()){
-                    case 1;
+                System.out.println("Tryk 2. for at vise pensumliste");
+                System.out.println("Tryk 3. for at opdatere dine brugeroplysninger");
+                System.out.println("Tryk 4. for at slette bruger");
+                switch (input.nextInt()) {
+                    case 1:
                         printBook();
                         break;
-                    
+                    case 2:
+                        //slet bruger
+
+                    default:
+                        System.out.println("Du bedes bruge én af valgmulighederne");
+
+
+
                 }
+            } while (true); //
                 else {
                     System.out.println("Forkert brugernavn eller adgangskode");
                 }
-            }
+        //Her er klassen til printBook. Den g
+        }
+
         }
     }
 
